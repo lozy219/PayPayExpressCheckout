39009544df95efbef24297585b05a461b3b0e632
Lei Mingyu <uygnim.iel@gmail.com>
2015-03-16 01:26:35 +0800

    demo test

---
+++ web/index.php
@@ -0,0 +1,33 @@
+<?php
+
+require('../vendor/autoload.php');
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register the Twig templating engine
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__.'/../views',
+));
+
+// Our web handlers
+
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return 'Hello';
+});
+
+$app->get('/twig/{name}', function ($name) use ($app) {
+    return $app['twig']->render('index.twig', array(
+        'name' => $name,
+    ));
+});
+
+$app->run();
+
+?>
