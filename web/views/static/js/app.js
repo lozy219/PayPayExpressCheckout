73fbdb4d5cafb73a5c4240c63f87338b07681db8
Lei Mingyu <uygnim.iel@gmail.com>
2015-03-16 22:23:26 +0800

    calculate total money

--- web/views/static/js/app.js
+++ web/views/static/js/app.js
@@ -1,5 +1,14 @@
 $(function() {
 	$('div[id^="book-"]').click(function () {
-        $(this).find(".book-price").html("ha");
+		var item = $(this).find(".book-price");
+		if (item.hasClass("selected")) {
+			item.removeClass("selected");
+			item.html(item.attr("data-price"));
+			$('#sum').html((parseFloat($('#sum').html()) - parseFloat(item.html())).toFixed(2));
+		} else {
+			item.addClass("selected");
+			$('#sum').html((parseFloat($('#sum').html()) + parseFloat(item.html())).toFixed(2));
+        	item.html('<i class="fa fa-check-circle"></i>');
+		}
     }); 
 });
