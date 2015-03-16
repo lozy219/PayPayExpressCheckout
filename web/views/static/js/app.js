c38f8e6202ad6716e802cc263ed33456d08b5e9a
Lei Mingyu <uygnim.iel@gmail.com>
2015-03-17 01:37:31 +0800

    before integrate with sdk

--- web/views/static/js/app.js
+++ web/views/static/js/app.js
@@ -4,7 +4,7 @@ $(function() {
 		if (item.hasClass("selected")) {
 			item.removeClass("selected");
 			item.html('<span style="font-size: 19px">S$ </span>' + item.attr("data-price"));
-			var sum = (parseFloat($('#sum').html()) - parseFloat(item.attr("data-price"))).toFixed(2)
+			var sum = (parseFloat($('#sum').html()) - parseFloat(item.attr("data-price"))).toFixed(2);
 			if (sum > 0) {
 				$('#checkout').removeAttr('disabled');
 			} else {
@@ -13,7 +13,7 @@ $(function() {
 			$('#sum').html(sum);
 		} else {
 			item.addClass("selected");
-			var sum = (parseFloat($('#sum').html()) + parseFloat(item.attr("data-price"))).toFixed(2)
+			var sum = (parseFloat($('#sum').html()) + parseFloat(item.attr("data-price"))).toFixed(2);
 			if (sum > 0) {
 				$('#checkout').removeAttr('disabled');
 			} else {
