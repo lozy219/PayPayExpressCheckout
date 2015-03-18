c70217c0e96ad16a51084b58fe967b8fb1efed05
Lei Mingyu <uygnim.iel@gmail.com>
2015-03-19 02:38:09 +0800

    navbar modi

--- web/TextBookController.php
+++ web/TextBookController.php
@@ -37,6 +37,7 @@ class TextBookController {
 		// }
 		// return $books;
 	}
+
 }
 
 ?>
--- web/views/static/js/app.js
+++ web/views/static/js/app.js
@@ -11,8 +11,10 @@ $(function() {
 			var sum = (parseFloat($('#sum').html()) - parseFloat(item.attr("data-price"))).toFixed(2);
 			if (sum > 0) {
 				$('#checkout').removeAttr('disabled');
+				$('#checkout-navbar').removeClass('navbar-disabled');
 			} else {
 				$('#checkout').attr('disabled', 'disabled');
+				$('#checkout-navbar').addClass('navbar-disabled');
 			}
 			$('#sum').html(sum);
 		} else {
@@ -20,8 +22,10 @@ $(function() {
 			var sum = (parseFloat($('#sum').html()) + parseFloat(item.attr("data-price"))).toFixed(2);
 			if (sum > 0) {
 				$('#checkout').removeAttr('disabled');
+				$('#checkout-navbar').removeClass('navbar-disabled');
 			} else {
 				$('#checkout').attr('disabled', 'disabled');
+				$('#checkout-navbar').addClass('navbar-disabled');
 			}
 			$('#sum').html(sum);
         	item.html('<i class="fa fa-check-circle"></i>');
@@ -29,25 +33,35 @@ $(function() {
     }); 
 
     $('#checkout').click(function () {
-    	$('div[id^="book-"]').each(function () {
-    		var id = $(this).attr('id').substr(5);
-    		if (is_selected(id)) {
-    			$('#order-' + id).show();
-    			$('#checkbox-' + id).attr('checked', true);
-    		}
-    	});
+    	checkout();
+    });
 
-    	var sum = parseFloat($('#sum').html());
-    	$('#modal-sum').html((sum * 1.07).toFixed(2));
+    $('#checkout-navbar').click(function() {
+    	if (!($(this).hasClass("navbar-disabled"))) {
+    		$('#confirm-order').modal('show'); 
+    		checkout();
+    	}
     });
 });
 
 function is_selected(id) {
 	var item = $('#book-' + id).find(".book-price");
-	console.log(item);
 	if (item.hasClass("selected")) {
 		return true;
 	} else {
 		return false;
 	}
+}
+
+function checkout() {
+	$('div[id^="book-"]').each(function () {
+		var id = $(this).attr('id').substr(5);
+		if (is_selected(id)) {
+			$('#order-' + id).show();
+			$('#checkbox-' + id).attr('checked', true);
+		}
+	});
+
+	var sum = parseFloat($('#sum').html());
+	$('#modal-sum').html((sum * 1.07).toFixed(2));
 }
